b4dd822a546588004b182598ea4673fc52b328b0
Ally Preston <APreston@spartaglobal.com>
2019-07-15 10:00:58 +0100

    create variable

---
+++ 01variables.py
@@ -0,0 +1,16 @@
+#variables in python
+
+#its like a box
+#it has a name
+#                it stores things inside
+my_variable = 'Hello there'
+
+#display variable that is a string
+print(my_variable)
+
+#reassign a variable
+
+my_variable = "I'm new content being reassigned to my_variable " + 'more thing'
+
+#print variable
+print(my_variable)
