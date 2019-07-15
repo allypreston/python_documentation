25222cecb515a2ab2ed1ac600a1c0a9e9cb344b7
Ally Preston <APreston@spartaglobal.com>
2019-07-15 13:37:23 +0100

    create variable, string, numerical, casting files with examples and operations. First example

---
+++ Exercise1.py
@@ -0,0 +1,27 @@
+#create a program that asks for the users name, the date and a funny fact
+
+
+# #asks user for name
+# first_name = input("What is your first name? > ")
+# last_name = input("What is your last name? > ")
+# full_name = first_name.capitalize() + ' ' + last_name.capitalize()
+# #asks user for date
+# print("what is an important date for you? > ")
+# date = input("please input YYYY-MM-DD > ")
+# #ask reason for the important date
+# date_reason = input("Why is that date important to you? > ")
+# #asks user for a funny fact
+# funny_fact = input("Could you give me a funny fact? > ")
+# # ask user favourite pokemon
+# favourite_pokemon = input("What is your favourite pokemon? > ")
+# print(f"Thank you for inputting the details {full_name}, your fact is very interesting.")
+# print(full_name)
+# print(date)
+# print(date_reason)
+# print(funny_fact)
+# print(favourite_pokemon)
+
+number = input("Could you give me a number? > ")
+number = int(number)
+number = number * 42
+print(number)
