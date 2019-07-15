3b93a9a00dea77776c47ad69f77d4ba4a0cb5bdc
Ally Preston <APreston@spartaglobal.com>
2019-07-15 12:09:46 +0100

    create variable, string, numerical, casting files with examples and operations

---
+++ 04casting.py
@@ -0,0 +1,8 @@
+# casting is when you change the datatype of an object
+# we can cast strings into integers and integers into strings
+
+#casting string class into integer class
+# change string into an integer
+my_string = '10'
+int(my_string)
+#change integer into a string
